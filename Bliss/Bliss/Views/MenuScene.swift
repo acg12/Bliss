64873861969739b54d4c6a10cba3dec698dc5c50
Angela Christabel <angela.christabel@gmail.com>
2023-05-25 22:12:11 +0700

    Menu scene done

--- Bliss/Bliss/Views/MenuScene.swift
+++ Bliss/Bliss/Views/MenuScene.swift
@@ -9,17 +9,40 @@ import Foundation
 import SpriteKit
 
 class MenuScene: SKScene {
-    var lblStart: SKLabelNode?
-    var backgroundSound: SKAudioNode!
+    var title: SKLabelNode!
+    var caption: SKLabelNode!
+    var start: SKLabelNode!
+    var startBtn: SKShapeNode!
     
     override func didMove(to view: SKView) {
-        lblStart = self.childNode(withName: "StartBtn") as? SKLabelNode
+        // set text to brown
+        title = self.childNode(withName: "Title") as? SKLabelNode
+        title.fontColor = UIColor(named: "Liver Brown")
+        title.zPosition = 1
         
-        // add audio
-        if let musicURL = Bundle.main.url(forResource: "Bliss BGM", withExtension: "mp3") {
-            backgroundSound = SKAudioNode(url: musicURL)
-            self.addChild(backgroundSound)
-        }
+        caption = self.childNode(withName: "Caption") as? SKLabelNode
+        caption.fontColor = UIColor(named: "Liver Brown")
+        caption.zPosition = 1
+        
+        start = self.childNode(withName: "StartText") as? SKLabelNode
+        start.fontColor = UIColor(named: "Liver Brown")
+        start.zPosition = 2
+        
+        // Make the button
+        let size = CGSize(width: 152, height: 43)
+        let cornerRadius: CGFloat = 100
+        let roundedRect = UIBezierPath(roundedRect: CGRect(origin: .zero, size: size), cornerRadius: cornerRadius)
+        startBtn = SKShapeNode(path: roundedRect.cgPath)
+
+        // Set the appearance of the shape
+        startBtn.fillColor = UIColor(named: "Gray White") ?? .white
+        startBtn.strokeColor = UIColor(named: "Liver Brown") ?? .black
+        startBtn.lineWidth = 3
+        startBtn.position.x = start.position.x - startBtn.frame.width/2
+        startBtn.position.y = start.position.y - startBtn.frame.height/2 + start.frame.height/2
+        startBtn.zPosition = 1
+        
+        self.addChild(startBtn)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -27,8 +50,7 @@ class MenuScene: SKScene {
             let nodes = self.nodes(at: touch.location(in: self))
             
             for node in nodes {
-                if node == lblStart {
-                    print("pressed start")
+                if node == startBtn {
                     if let scene = SKScene(fileNamed: "GameScene") {
                         // Set the scale mode to scale to fit the window
                         scene.scaleMode = .aspectFill
